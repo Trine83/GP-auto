dae913a0d50adb95bca5ebc93e311f667dd291d2
Trine83 <trine.andresen83@outlook.com>
2023-05-31 13:25:51 +0200

    chat on click

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+function init() {
+    let chatContainer = document.querySelector(".chat-wrapper");
+    let chatToggle = document.querySelector(".chat-toggle");
+    chatToggle.addEventListener('click', () => {
+        chatContainer.classList.toggle('hidden');
+    })
+}
+
+init();
+
+
+  
